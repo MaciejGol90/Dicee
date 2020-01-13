94d8dac9f6a1f421330fb3d28781d6d7f0fa88d2
Maciej Golianek <maciek.golianek@gmail.com>
2020-01-13 12:02:55 +0100

    random roll fix

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
+// finsh by Maciej Gol on 13/01/2020
 //
 
 import UIKit
@@ -17,11 +18,12 @@ class ViewController: UIViewController {
     
         let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView1.image = diceImages[Int.random(in: 0...5)]
-        diceImageView2.image = diceImages[Int.random(in: 0...5)]
+        diceImageView1.image = diceImages.randomElement()
+        diceImageView2.image = diceImages.randomElement()
       
     }
     
 
 }
 
+ 
